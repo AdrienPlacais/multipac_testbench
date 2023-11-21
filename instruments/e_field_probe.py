e4939f6793bec859d8e27db85923133a651acd2c
Adrien P <placais@lpsc.in2p3.fr>
2023-11-21 13:36:25 +0100

    set structure for when I will have numerical mp limits

--- instruments/e_field_probe.py
+++ instruments/e_field_probe.py
@@ -13,8 +13,11 @@ class ElectricFieldProbe(Instrument):
     """A probe to measure electric field."""
 
     y_label: str = r"Voltage $[V]$"
+    numerical_e_limits: np.ndarray | None = None
 
     @property
     def mp_indexes(self) -> np.ndarray[np.int64]:
-        """Determine index of measurements where MP was detected."""
-        raise IOError("No detection of multipactor for {type(self)}.")
+        """Determine index of measurements where MP should be detected."""
+        assert self.numerical_e_limits is not None, "You should give MP "\
+            "limits to allow comparison."
+        raise NotImplementedError("to do")
