fd4ce576119839730f8d0080708b2a5ed5cf9204
Adrien P <placais@lpsc.in2p3.fr>
2024-01-16 16:25:49 +0100

    possible to add the transfer function of the probe, as well as rack correction constants

--- src/instruments/electric_field/field_probe.py
+++ src/instruments/electric_field/field_probe.py
@@ -7,3 +7,28 @@ from multipac_testbench.src.instruments.electric_field.i_electric_field import\
 
 class FieldProbe(IElectricField):
     """A probe to measure electric field."""
+
+    def __init__(self,
+                 *args,
+                 g_probe: float | None = None,
+                 a_rack: float | None = None,
+                 b_rack: float | None = None,
+                 **kwargs) -> None:
+        r"""Instantiate with some specific arguments.
+
+        Parameters
+        ----------
+        g_probe : float | None, optional
+            Total attenuation. Probe specific, also depends on frequency. The
+            default is None.
+        a_rack : float | None, optional
+            Rack calibration slope in :math:`\mathrm{V/dBm}`. The default is
+            None.
+        b_rack : float | None, optional
+            Rack calibration constant in :math:`\mathrm{dBm}`. The default is
+            None.
+
+        """
+        super().__init__(*args, **kwargs)
+        self._g_probe = g_probe
+        self._a_rack, self._b_rack = a_rack, b_rack
