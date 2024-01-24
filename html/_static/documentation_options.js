39e6080f3cb40bed777f1c67f3428d2edf762e2d
Adrien P <placais@lpsc.in2p3.fr>
2024-01-24 10:06:17 +0100

    with plot of multipacting limits

--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1.0-15-g9cb3a0e',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.1.1-9-g9317046',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
