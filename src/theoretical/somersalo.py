af4cbd4b4bde9b06a9c08d2d4db64d5ce87c031e
Adrien P <placais@lpsc.in2p3.fr>
2024-02-02 10:18:04 +0100

    somersalo theoretical can be returned as dict to ease conversion to pandas

--- src/theoretical/somersalo.py
+++ src/theoretical/somersalo.py
@@ -10,11 +10,26 @@ In : Part. Accel. 59 (1998), p. 107-141. \
 url : http://cds.cern.ch/record/1120302/files/p107.pdf.
 
 """
+from typing import overload
 import numpy as np
 
 
+@overload
 def one_point(log_power: np.ndarray,
-              order: int) -> tuple[np.ndarray, np.ndarray]:
+              order: int,
+              to_dict: bool = True) -> dict[str, np.ndarray]: ...
+
+
+@overload
+def one_point(log_power: np.ndarray,
+              order: int,
+              to_dict: bool = False) -> tuple[np.ndarray, np.ndarray]: ...
+
+
+def one_point(log_power: np.ndarray,
+              order: int,
+              to_dict: bool = False
+              ) -> tuple[np.ndarray, np.ndarray] | dict[str, np.ndarray]:
     r"""Compute one-point multipactor bands of order ``order``.
 
     Parameters
@@ -26,18 +41,38 @@ def one_point(log_power: np.ndarray,
 
     Returns
     -------
-    np.ndarray, np.ndarray
+    np.ndarray, np.ndarray | dict[str, np.ndarray]
         Lower and upper multipactor limits, in :math:`(\mathrm{GHz} \times
         \mathrm{mm})^4 \times \Omega`.
 
     """
     low = 3. * log_power
     upp = 3.5 * log_power
+    if to_dict:
+        out = {
+            f'One-point order {order} (lower lim)': low,
+            f'One-point order {order} (upper lim)': upp,
+        }
+        return out
     return low, upp
 
 
+@overload
 def two_point(log_power: np.ndarray,
-              order: int) -> tuple[np.ndarray, np.ndarray]:
+              order: int,
+              to_dict: bool = True) -> dict[str, np.ndarray]: ...
+
+
+@overload
+def two_point(log_power: np.ndarray,
+              order: int,
+              to_dict: bool = False) -> tuple[np.ndarray, np.ndarray]: ...
+
+
+def two_point(log_power: np.ndarray,
+              order: int,
+              to_dict: bool = False
+              ) -> tuple[np.ndarray, np.ndarray] | dict[str, np.ndarray]:
     r"""Compute two-point multipactor bands of order ``order``.
 
     Parameters
@@ -49,11 +84,18 @@ def two_point(log_power: np.ndarray,
 
     Returns
     -------
-    np.ndarray, np.ndarray
+    np.ndarray, np.ndarray | dict[str, np.ndarray]
         Lower and upper multipactor limits, in :math:`(\mathrm{GHz} \times
         \mathrm{mm})^4 \times \Omega^2`.
 
     """
     low = 4. * log_power
     upp = 4.5 * log_power
+
+    if to_dict:
+        out = {
+            f'Two-point order {order} (lower lim)': low,
+            f'Two-point order {order} (upper lim)': upp,
+        }
+        return out
     return low, upp
