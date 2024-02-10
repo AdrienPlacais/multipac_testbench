2a53646bf5be2c5a2684870d0b401074ff9d1748
Adrien P <placais@lpsc.in2p3.fr>
2024-02-10 18:53:05 +0100

    added a method to auto patch g_probe error

--- src/instruments/electric_field/field_probe.py
+++ src/instruments/electric_field/field_probe.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Define field probe to measure electric field."""
+from functools import partial
 from multipac_testbench.src.instruments.electric_field.i_electric_field import\
     IElectricField
+from multipac_testbench.src.util.post_treaters import (v_acquisition_to_v_coax,
+                                                       v_coax_to_v_acquisition)
 
 
 class FieldProbe(IElectricField):
@@ -13,6 +16,7 @@ class FieldProbe(IElectricField):
                  g_probe: float | None = None,
                  a_rack: float | None = None,
                  b_rack: float | None = None,
+                 patch: bool = False,
                  **kwargs) -> None:
         r"""Instantiate with some specific arguments.
 
@@ -32,8 +36,28 @@ class FieldProbe(IElectricField):
         super().__init__(*args, **kwargs)
         self._g_probe = g_probe
         self._a_rack, self._b_rack = a_rack, b_rack
+        if patch:
+            self._patch_data()
 
     @classmethod
     def ylabel(cls) -> str:
         """Label used for plots."""
         return r"Measured voltage [V]"
+
+    def _patch_data(self, g_probe_in_labview: float = 1.) -> None:
+        """Correct when ``g_probe`` in LabVIEWER is wrong."""
+        assert self._a_rack is not None
+        assert self._b_rack is not None
+        assert self._g_probe is not None
+        fun1 = partial(v_coax_to_v_acquisition,
+                       g_probe=g_probe_in_labview,
+                       a_rack=self._a_rack,
+                       b_rack=self._b_rack,
+                       z_0=50.)
+        fun2 = partial(v_acquisition_to_v_coax,
+                       g_probe=self._g_probe,
+                       a_rack=self._a_rack,
+                       b_rack=self._b_rack,
+                       z_0=50.)
+        self.add_post_treater(fun1)
+        self.add_post_treater(fun2)
