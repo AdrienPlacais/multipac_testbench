c4a4260f18b7d16689e86c42a88af3b33bc9be0e
Adrien P <placais@lpsc.in2p3.fr>
2024-02-11 11:06:09 +0100

    frequency is also saved

--- src/util/helper.py
+++ src/util/helper.py
@@ -8,11 +8,12 @@ from pathlib import Path
 
 def output_filepath(filepath: Path,
                     swr: float,
+                    freq_mhz: float,
                     out_folder: str,
                     extension: str,
                     ) -> Path:
     """Return a new path to save output files."""
-    filename = filepath.with_stem(f"{swr}_"
+    filename = filepath.with_stem(f"swr{swr}_freq{freq_mhz}"
                                   + filepath.stem).with_suffix(extension).name
     folder = filepath.parent / out_folder
     if not folder.is_dir():
