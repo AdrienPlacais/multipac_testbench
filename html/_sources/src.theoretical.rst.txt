25e8181e2c4e19a2ce11b65e49367b3baeb0bea1
Adrien P <placais@lpsc.in2p3.fr>
2024-03-02 14:27:13 +0100

    recompiled for 1.5.1

--- html/_sources/src.theoretical.rst.txt
+++ html/_sources/src.theoretical.rst.txt
@@ -13,4 +13,3 @@ Submodules
    :maxdepth: 4
 
    src.theoretical.somersalo
-   src.theoretical.susceptibility
--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.5.0',
+    VERSION: '1.5.0-9-g4d67c55',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
