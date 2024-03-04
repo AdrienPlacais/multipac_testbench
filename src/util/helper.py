632bc1c8de3a521587b00709dd234f14ad72dcb9
Adrien P <placais@lpsc.in2p3.fr>
2024-03-04 12:35:50 +0100

    output filepaths are more explicit

--- src/util/helper.py
+++ src/util/helper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Define general usage functions."""
+import logging
 from pathlib import Path
 
 import numpy as np
@@ -9,15 +10,46 @@ import numpy as np
 def output_filepath(filepath: Path,
                     swr: float,
                     freq_mhz: float,
-                    out_folder: str,
+                    out_folder: str | Path,
                     extension: str,
                     ) -> Path:
-    """Return a new path to save output files."""
-    filename = filepath.with_stem(f"swr{swr}_freq{freq_mhz}"
-                                  + filepath.stem).with_suffix(extension).name
+    """Return a new path to save output files.
+
+    Parameters
+    ----------
+    filepath : Path
+        Name of the data ``.csv`` file from LabViewer.
+    swr : float
+        Theoretical :math:`SWR` to add to the output file name.
+    freq_mhz : float
+        Theoretical rf frequency to add to the output file name.
+    out_folder : str | Path
+        Relative name of the folder where data will be saved; it is defined
+        w.r.t. to the parent folder of ``filepath``.
+    extension : str
+        Extension of the output file, with the dot.
+
+    Returns
+    -------
+    Path
+        A full filepath.
+
+    """
+    if np.isinf(swr):
+        swr_str = "SWR_infty"
+    else:
+        swr_str = f"SWR_{int(swr):05.0f}"
+    freq_str = f"freq_{freq_mhz:03.0f}MHz"
+
+    filename = filepath.with_stem(
+        ('_').join((swr_str, freq_str, filepath.stem))
+    ).with_suffix(extension).name
+
     folder = filepath.parent / out_folder
+
     if not folder.is_dir():
         folder.mkdir(parents=True)
+
     return folder / filename
 
 
