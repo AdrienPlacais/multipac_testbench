1d9ec18bf522f761ae0b3132f59908f96b0de5d9
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-06-25 09:57:39 +0200

    more generic typing

--- src/multipac_testbench/multipactor_band/campaign_multipactor_bands.py
+++ src/multipac_testbench/multipactor_band/campaign_multipactor_bands.py
@@ -1,5 +1,7 @@
 """Store all the :class:`.TestMultipactorBands` of several tests."""
 
+from collections.abc import Sequence
+
 from multipac_testbench.multipactor_band.test_multipactor_bands import (
     TestMultipactorBands,
 )
@@ -9,7 +11,7 @@ class CampaignMultipactorBands(list):
     """Hold multipactor bands measured during several tests."""
 
     def __init__(
-        self, tests_multipactor_bands: list[TestMultipactorBands | None]
+        self, tests_multipactor_bands: Sequence[TestMultipactorBands | None]
     ) -> None:
         """Instantiate the object."""
         super().__init__(tests_multipactor_bands)
