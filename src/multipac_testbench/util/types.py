0934d28229a80629b90e5b0bc5690b1ba8e1a061
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-07-17 09:40:41 +0200

    reordering of types

--- src/multipac_testbench/util/types.py
+++ src/multipac_testbench/util/types.py
@@ -6,14 +6,6 @@ import numpy as np
 import pandas as pd
 from numpy.typing import NDArray
 
-#: A function that takes in the data of an instrument and returns an array with
-#: same shape
-POST_TREATER_T = Callable[[NDArray[np.float64]], NDArray[np.float64]]
-
-#: A function that takes in an :class:`.Instrument` data and return a boolean
-#: array with same shape, indicating whether multipactor appeared
-MULTIPAC_DETECTOR_T = Callable[[NDArray[np.float64]], NDArray[np.bool]]
-
 #: Function/method to call when a post-treater is added to an
 #: :class:`.Instrument`.
 #:
@@ -25,3 +17,11 @@ CALLBACK_T = Callable[[], pd.Series]
 #: Electric field probes names
 FIELD_PROBES = ("E1", "E2", "E3", "E4", "E5", "E6", "E7")
 FIELD_PROBES_T = Literal["E1", "E2", "E3", "E4", "E5", "E6", "E7"]
+
+#: A function that takes in an :class:`.Instrument` data and return a boolean
+#: array with same shape, indicating whether multipactor appeared
+MULTIPAC_DETECTOR_T = Callable[[NDArray[np.float64]], NDArray[np.bool]]
+
+#: A function that takes in the data of an instrument and returns an array with
+#: same shape
+POST_TREATER_T = Callable[[NDArray[np.float64]], NDArray[np.float64]]
